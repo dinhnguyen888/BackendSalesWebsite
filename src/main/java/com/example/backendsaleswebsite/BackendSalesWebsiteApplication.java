105eaae59003a06f7d315ef01e33126961d02b96
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2024-10-03 21:25:51 +0700

    first commit

---
+++ src/main/java/com/example/backendsaleswebsite/BackendSalesWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendsaleswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSalesWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSalesWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendsaleswebsite/BackendSalesWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendsaleswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSalesWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
