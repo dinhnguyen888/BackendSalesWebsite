16399a4c81917cc732412e0bbd73e96e40aac41d
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2024-10-05 16:53:32 +0700

    add swagger config and first run

---
+++ src/main/java/com/example/backendsaleswebsite/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.backendsaleswebsite.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Cho phép tất cả yêu cầu
+                )
+                .csrf(AbstractHttpConfigurer::disable); // Tắt CSRF nếu cần
+
+        return http.build();
+    }
+}
