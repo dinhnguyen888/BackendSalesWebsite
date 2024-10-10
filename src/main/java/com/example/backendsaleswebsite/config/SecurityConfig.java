9cd1241942fcc364f516d6ce8b34369c9ef2a693
NguyenQuocKhanh <Raycrop555@gmail.com>
2024-10-10 20:29:42 +0700

    Spring security config

--- src/main/java/com/example/backendsaleswebsite/config/SecurityConfig.java
+++ src/main/java/com/example/backendsaleswebsite/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.backendsaleswebsite.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,21 +9,62 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private final String[] END_POINT = {"/api/v1/auth/**", "/v3/api-docs/**", "/swagger-ui/**",
+										"/login", "/register", "/introspect"};
+	
+	private String SIGNER_KEY = "GaHQG6SjkCOQBF5yspA4Bd+t1EGA1gP+UP++0odDou9MUNdArwKwCX1kmqtSlEhQ";
+	
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .csrf(AbstractHttpConfigurer::disable);
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    	httpSecurity
+			.authorizeHttpRequests(request -> 
+			request.requestMatchers(END_POINT).permitAll()
+			.anyRequest().authenticated());
+    	
+    	httpSecurity
+    		.oauth2ResourceServer(oauth2 ->
+    		oauth2.jwt(JwtConfigurer -> JwtConfigurer.decoder(jwtDecoder())));
+    	
+    	httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
-        return http.build();
+        return httpSecurity.build();
+    }
+    
+    @Bean
+    JwtDecoder jwtDecoder() {
+    	SecretKeySpec secretKeySpec = new SecretKeySpec(SIGNER_KEY.getBytes(), "HS512");
+    			
+    	return NimbusJwtDecoder
+    					.withSecretKey(secretKeySpec)
+    					.macAlgorithm(MacAlgorithm.HS512)
+    					.build();
+    }
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("API").version("1.0"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
     }
     
     @Bean
--- src/main/java/com/example/backendsaleswebsite/controller/AuthController.java
+++ src/main/java/com/example/backendsaleswebsite/controller/AuthController.java
@@ -3,24 +3,36 @@ package com.example.backendsaleswebsite.controller;
 import com.example.backendsaleswebsite.dto.ApiResponse;
 import com.example.backendsaleswebsite.dto.AuthenticationRequest;
 import com.example.backendsaleswebsite.dto.AuthenticationResponse;
+import com.example.backendsaleswebsite.dto.IntrospectRequest;
+import com.example.backendsaleswebsite.dto.IntrospectResponse;
 import com.example.backendsaleswebsite.dto.LoginRequest;
 import com.example.backendsaleswebsite.dto.RegisterRequest; // Đừng quên import RegisterRequest
 import com.example.backendsaleswebsite.service.AuthService;
+import com.nimbusds.jose.JOSEException;
+
+import java.text.ParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 public class AuthController {
 
 	@Autowired
 	private AuthService authService;
 	
-	@PostMapping("/token")
+	@PostMapping("/login")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         var result = authService.login(request);
         return ApiResponse.<AuthenticationResponse>builder().result(result).build();
+    }
+	
+	@PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request) 
+    		throws JOSEException, ParseException {
+        var result = authService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder().result(result).build();
     }
 
     @PostMapping("/register")
