1f6c9cc1c3dc241e9fb1951451b9867729cd9805
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2024-10-16 22:42:26 +0700

    test Cors config

--- src/main/java/com/example/backendsaleswebsite/config/WebConfig.java
+++ src/main/java/com/example/backendsaleswebsite/config/WebConfig.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("http://localhost:3000")  // Thay địa chỉ frontend của bạn
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/*")
+//                .allowedOrigins("http://localhost:3000")  // Thay địa chỉ frontend của bạn
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("")
+//                .allowCredentials(true);
+//    }
 }
